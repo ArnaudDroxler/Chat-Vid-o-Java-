31e4642c8d26b5afb36a1a521abbee2196c9d932
maxime.piergiov <maxime.piergio@he-arc.ch>
2016-05-18 15:48:13 +0200

    On capture la video sans la classe toute faite

--- ChatVideo/src/ch/hearc/chatvideo/testvideo/JFrameTestVideo.java
+++ ChatVideo/src/ch/hearc/chatvideo/testvideo/JFrameTestVideo.java
@@ -1,33 +1,74 @@
 
 package ch.hearc.chatvideo.testvideo;
 
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
 
-public class JFrameTestVideo
+public class JFrameTestVideo extends JFrame
 	{
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Constructeurs							*|
 	\*------------------------------------------------------------------*/
-	
+
+	public JFrameTestVideo()
+		{
+		geometry();
+		control();
+		appearance();
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
-	
+
 	/*------------------------------*\
 	|*				Set				*|
 	\*------------------------------*/
-	
+
 	/*------------------------------*\
 	|*				Get				*|
 	\*------------------------------*/
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
-	
+
+	private void geometry()
+		{
+		// JComponent : Instanciation
+		panel = new JPanelTestVideo();
+		// Layout : Specification
+			{
+			BorderLayout borderLayout = new BorderLayout();
+			setLayout(borderLayout);
+
+			// borderLayout.setHgap(20);
+			// borderLayout.setVgap(20);
+			}
+
+		// JComponent : add
+		add(panel);
+		}
+
+	private void control()
+		{
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		}
+
+	private void appearance()
+		{
+		setSize(600, 400);
+		setLocationRelativeTo(null); // frame centrer
+		setVisible(true); // last!
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Attributs Private						*|
 	\*------------------------------------------------------------------*/
-	}
 
+	// Tools
+	JPanelTestVideo panel;
+
+	}
--- ChatVideo/src/ch/hearc/chatvideo/testvideo/JPanelTestVideo.java
+++ ChatVideo/src/ch/hearc/chatvideo/testvideo/JPanelTestVideo.java
@@ -1,33 +1,134 @@
 
 package ch.hearc.chatvideo.testvideo;
 
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 
+import javax.swing.JPanel;
 
-public class JPanelTestVideo
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+
+public class JPanelTestVideo extends JPanel
 	{
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Constructeurs							*|
 	\*------------------------------------------------------------------*/
-	
+
+	public JPanelTestVideo()
+		{
+		webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		webcam.open();
+		geometry();
+		control();
+		appearance();
+		animate();
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
-	
+
 	/*------------------------------*\
 	|*				Set				*|
 	\*------------------------------*/
-	
+
 	/*------------------------------*\
 	|*				Get				*|
 	\*------------------------------*/
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
-	
+
+	private void animate()
+		{
+		Thread thread = new Thread(new Runnable()
+			{
+
+			@Override
+			public void run()
+				{
+				while(true)
+					{
+					if (webcam.isOpen())
+						{
+						repaint();
+						}
+					}
+
+				}
+			});
+		thread.start();
+		}
+
+	@Override
+	protected void paintComponent(Graphics g)
+		{
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		AffineTransform oldContext = g2d.getTransform();
+		draw(g2d);
+		g2d.setTransform(oldContext);
+		}
+
+	private void draw(Graphics2D g2d)
+		{
+		if (webcam.isImageNew())
+			{
+			BufferedImage img = webcam.getImage();
+			g2d.drawImage(img, 0, 0, null);
+			}
+		}
+
+	private void geometry()
+		{
+		// JComponent : Instanciation
+
+		// Layout : Specification
+			{
+			FlowLayout flowlayout = new FlowLayout(FlowLayout.CENTER);
+			setLayout(flowlayout);
+
+			// flowlayout.setHgap(20);
+			// flowlayout.setVgap(20);
+			}
+
+		// JComponent : add
+
+		}
+
+	private void control()
+		{
+		addComponentListener(new ComponentAdapter()
+			{
+
+			@Override
+			public void componentResized(ComponentEvent e)
+				{
+
+				}
+
+			});
+
+		}
+
+	private void appearance()
+		{
+		// rien
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Attributs Private						*|
 	\*------------------------------------------------------------------*/
-	}
 
+	// Tools
+	private Webcam webcam;
+	}
